09a1c62add96b25a6680e72b19e54d0262e6c063
KeYunTinG <zaac2240632@yahoo.com.tw>
2022-11-13 18:19:33 +0800

    refector: modify mongoose config

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -46,14 +46,6 @@ router.delete('/:_id', (req, res) => {
         .catch(error => console.log(error))
 })
 
-router.get('/search', (req, res) => {
-    const keyword = req.query.keyword
-    const restaurant = restaurantList.results.filter(restaurant =>
-        restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-        restaurant.category.includes(keyword)
-    )
-    res.render('index', { restaurants: restaurant, keyword: keyword })
-})
 
 
 module.exports = router;
