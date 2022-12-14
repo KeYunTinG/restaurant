cfeeb892b1b8c68aeecc7d9eae05ea5b184b6da5
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-12-14 11:48:09 +0800

    feat: add logout feature

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const app = express()
 const PORT = process.env.PORT || 3000
 require('./config/mongoose')
 
@@ -14,6 +14,11 @@ require('./config/mongoose')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const User = require('../../models/user')
+const passport = require('passport')
 const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
 router.post('/login', (req, res) => {
+})
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 router.get('/register', (req, res) => {
   res.render('register')
