e9d8b8f0e4f69c8da9de079cccf9d9baf82ea665
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-12-15 11:56:09 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const usePassport = require('./config/passport')
 const routes = require('./routes')
+const flash = require('connect-flash')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 require('./config/mongoose')
@@ -24,6 +26,7 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
